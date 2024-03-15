d95e48dcff2f5a74b04048f3c3600e7d6340399c
laureano magallanes <laureano.magallanes.98@gmail.com>
2024-03-15 10:25:01 -0300

    adding nginx to main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,9 +14,8 @@ provider "docker" {
 resource "docker_image" "app_image" {
   name = "test1"
   build {
-##need to change this to a variable path
-    context = "/home/magallanes/Documentos/Quansight/interview-assignment-infra-laumagallanes/app/"
-    dockerfile = "" #/home/magallanes/Documentos/Quansight/interview-assignment-infra-laumagallanes/app/Dockerfile"
+    context = "/home/magallanes/Documentos/Quansight/resolucion/app/" #change tha path to the location of your repository
+    dockerfile = ""
   }
 }
 
@@ -27,10 +26,35 @@ resource "docker_container" "app_container" {
     internal = 8000
   }
   volumes {
-    host_path      = "/home/magallanes/Documentos/Quansight/interview-assignment-infra-laumagallanes/app/src"
+    host_path      = "/home/magallanes/Documentos/Quansight/resolucion/app/src" #same here
     container_path = "/app"
   }
   depends_on  = [docker_image.app_image]
   network_mode = "bridge"
 }
+
+resource "docker_image" "nginx_image" {
+  name = "nginxq1"
+build {
+     context =  "/home/magallanes/Documentos/Quansight/resolucion/nginx/"
+dockerfile = ""
+      }
+}
+
+resource "docker_container" "nginx_container" {
+  name  = "nginx_container"
+  image = "nginxq1"
+  ports {
+    external = 80 
+    internal = 80
+ } 
+  ports {
+    external = 443
+    internal = 443 
+  }
+
+depends_on  = [docker_image.nginx_image, docker_container.app_container]
+  network_mode = "bridge"
+}
+
 
